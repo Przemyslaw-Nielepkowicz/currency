f1b93e53f65dba349ff073796b6d0da396a88cfe
Przemyslaw Nielepkowicz <nielepkowicz.przemyslaw@gmail.com>
2023-03-01 20:57:26 +0100

    reset button clears sum

--- js/script.js
+++ js/script.js
@@ -1,53 +1,51 @@
-let selectElement = document.querySelector(".js-select");
+let currencyElement = document.querySelector(".js-select");
 let amountElement = document.querySelector(".js-amount");
 let formElement = document.querySelector(".js-form");
 let sumElement = document.querySelector(".js-sum");
+let resetElement = document.querySelector(".js-reset")
 
-let EUR = 4.7245
-let USD = 4.463
-let GBP = 5.3663
-let CNY = 0.6433
-let JPY = 0.033
-
-amountElement.focus();
+let EUR = 4.7245;
+let USD = 4.463;
+let GBP = 5.3663;
+let CNY = 0.6433;
+let JPY = 0.033;
 
 formElement.addEventListener("reset", () => {
     console.log("Formularz zostal zresetowany");
 });
 
+resetElement.addEventListener("click", () => {
+    sumElement.innerText = "N/A";                        
+});
+
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let select = selectElement.value;
+    let currency = currencyElement.value;
     let amount = +amountElement.value;
     let sum;
 
-    switch (select) {
+    switch (currency) {
         case "EUR": 
-        sum = amount / EUR;
-        break;
+            sum = amount / EUR;
+            break;
 
         case "USD": 
-        sum = amount / USD;
-        break;   
+            sum = amount / USD;
+            break;   
 
         case "GBP": 
-        sum = amount / GBP;
-        break; 
+            sum = amount / GBP;
+            break; 
 
         case "CNY": 
-        sum = amount / CNY;
-        break;
+            sum = amount / CNY;
+            break;
 
         case "JPY": 
-        sum = amount / JPY;
-        break;
+            sum = amount / JPY;
+            break;
     }
 
-    sumElement.innerHTML =(`${sum.toFixed(2)} ${select}`);
+    sumElement.innerHTML = `${sum.toFixed(2)} ${currency}`;
 });
-
-sumElement.addEventListener("reset", () => {           // Why this is not working? 
-    console.log("Formularz zostal zresetowany");       // Fix it later
-});
-
